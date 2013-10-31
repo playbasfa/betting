0d256dcda0be0dbdc11fa55828940f3aec01b3b2
playbasfa <playbasfa@gmail.com>
2013-10-31 18:40:41 +0100

    tutto

--- src/odds_it/To_db.java
+++ src/odds_it/To_db.java
@@ -1,6 +1,9 @@
 package odds_it;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.*;
 import java.math.BigDecimal;
@@ -27,16 +30,16 @@ public class To_db
     public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, MalformedURLException, JAXBException, IOException, InterruptedException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException, SQLException
       {
         To_db obj = new To_db();
-        obj.takeDate();
-        obj.todb(obj.objectize("../betting/src/files_xml/odds_it.xml"));
+        obj.todb(obj.objectize("/Users/fabrizio/Desktop/odds_it_2.xml"));
+     //   obj.todb(obj.objectize("../betting/src/files_xml/odds_it.xml"));
+        
       }
-    private String takeDate(){
-    	File f = new File(name);
-    	return date;
-    }
+  
+    	
     
     private void todb(Sports obj) throws SQLException
     {
+    	//System.out.println("ECCO"+obj.getFileDate());
     	conn = openConn();  	
     	String i_sport=null;
     	String n_sport=null;
@@ -48,7 +51,7 @@ public class To_db
             // Scrivi su DB Sport
             i_sport = lspo.get(a).getId().toString();
             n_sport = lspo.get(a).getName();
-            toSport(n_sport,i_sport);
+           toSport(n_sport,i_sport);
             
             ///////////////////////
             
@@ -74,7 +77,8 @@ public class To_db
                     String sd_match = lmat.get(c).getStartDate();
                     String l_match = lmat.get(c).getLiveId();
                     String st_match = lmat.get(c).getStreaming().toString();
-                    toMatch(i_match,n_match,sd_match,l_match,st_match,i_event);
+                    
+                   toMatch(i_match,n_match,sd_match,l_match,st_match,i_event);
                     ///////////////////////
             
                     List<Bet> lbet = obj.getSport().get(a).getEvent().get(b).getMatch().get(c).getBets().getBet();
@@ -95,7 +99,9 @@ public class To_db
                             String n_choice = lcho.get(e).getName();
                             String i_choice = lcho.get(e).getId().toString();
                             BigDecimal o_choice = lcho.get(e).getOdd();
-                            toChoice(n_choice,i_choice,o_choice,i_bet);
+                           String xml_date=obj.getFileDate();
+                            toChoice(n_choice,i_choice,o_choice,i_bet,xml_date);
+                            //System.out.print(obj.getFileDate());
                             
                         }                
                     }
@@ -143,44 +149,72 @@ public class To_db
         }
     	return conn;
     }
-    private void toSport(String n_sport, String i_sport) throws SQLException{
+    private void toSport(String n_sport, String i_sport) {
     	String query="INSERT INTO" + " SPORTS"+ " VALUES(?,?)";
-        statement = conn.prepareStatement(query);
-        statement.setString(1, n_sport);
-        statement.setString(2, i_sport);
-        statement.executeUpdate();
-        statement.close();
+        
+        try{
+        	statement = conn.prepareStatement(query);
+            statement.setString(1, n_sport);
+            statement.setString(2, i_sport);
+        	statement.executeUpdate();
+        	statement.close();
+        }catch(SQLException e){
+        }
+        
     }
     private void toEvent(String n_event, String i_event, String i_sport) throws SQLException{
     	String query="INSERT INTO" + " EVENTS"+ " VALUES(?,?,?)";
-        statement = conn.prepareStatement(query);
+        try{
+    	statement = conn.prepareStatement(query);
         statement.setString(1, n_event);
         statement.setString(2, i_event);
         statement.setString(3, i_sport);
         statement.executeUpdate();
-        statement.close();
+        statement.close();}
+        catch(SQLException e){
+        }
     }
-    private void toMatch(String i_match,String n_match,String sd_match,String l_match,String st_match,String i_event){}
+    private void toMatch(String i_match,String n_match,String sd_match,String l_match,String st_match,String i_event){
+    	String query="INSERT INTO" + " EVENTS"+ " VALUES(?,?,?,?,?,?)";
+        try{
+    	statement = conn.prepareStatement(query);
+        statement.setString(1, n_match);
+        statement.setString(2, i_match);
+        statement.setString(3, sd_match);
+        statement.setString(4, l_match);
+        statement.setString(5, st_match);
+        statement.setString(6, i_event);
+        statement.executeUpdate();
+        statement.close();}
+        catch(SQLException e){
+        }
+    }
     private void toBet(String n_bet,String c_bet,String i_bet,String i_match) throws SQLException{
     	 String query="INSERT INTO" + " BETS"+ " VALUES(?,?,?,?)";
-        statement = conn.prepareStatement(query);
+        try{
+    	statement = conn.prepareStatement(query);
         statement.setString(1, n_bet);
         statement.setString(2, c_bet);
         statement.setString(3, i_bet);
         statement.setString(4, i_match);
         statement.executeUpdate();
-        statement.close();
+        statement.close();}
+        catch(SQLException e){
+        }
     }
-    private void toChoice(String n_choice,String i_choice,BigDecimal o_choice,String i_bet) throws SQLException{
-   	 String query="INSERT INTO" + " BETS"+ " VALUES(?,?,?,?)";
-   	query="INSERT INTO" + " CHOICES"+ " VALUES(?,?,?,?)";
-    statement = conn.prepareStatement(query);
+    private void toChoice(String n_choice,String i_choice,BigDecimal o_choice,String i_bet, String xml_date) throws SQLException{
+   	String query="INSERT INTO" + " CHOICES"+ " VALUES(?,?,?,?,?)";
+    try{
+   	statement = conn.prepareStatement(query);
     statement.setString(1, n_choice);
     statement.setString(2, i_choice);
     statement.setBigDecimal(3, o_choice);
     statement.setString(4, i_bet);
+    statement.setString(5, xml_date);
     statement.executeUpdate();
-    statement.close();
+    statement.close();}
+    catch(SQLException e){
+    }
    }
     private void closeConn()
     {
